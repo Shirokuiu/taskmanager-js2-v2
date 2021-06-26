35bd7becde74e7159ffda09ad57d50356d48c29f
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2021-06-26 18:05:43 +0300

    Добавил конфигурацию

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,3 @@
 import 'normalize.css';
 import './style.scss';
-// import './app/index';
-
-// const words = [
-//   'banana',
-//   'grapefruit',
-//   'orange',
-//   'banana',
-//   'orange',
-//   'grapefruit',
-//   'orange',
-//   'banana',
-//   'orange',
-//   'banana',
-//   'orange',
-// ];
-
-// const obj = {
-//   val: 10,
-//   getVal: function () {
-//     setTimeout(function () {
-//       console.log(this.val);
-//     }, 1000);
-//   },
-// };
-//
-// obj.getVal();
+import './app/index';
